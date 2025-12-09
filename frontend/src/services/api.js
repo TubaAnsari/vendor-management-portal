991d64c502ac06e8f2d7cd1ae7a45933a9db0ad8
TubaAnsari <tuba.ansari4321@gmail.com>
2025-12-09 19:57:11 +0530

    Initial commit: Vendor Management Portal

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
 const API_URL = import.meta.env.PROD 
-    ? 'https://vendor-management-portal.onrender.com/'
+    ? 'https://vendor-management-portal.onrender.com'
     : 'http://localhost:5000/api';
+
 const api = axios.create({
     baseURL: API_URL,
     headers: {
